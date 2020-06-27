d30d3ce1389ca3b06ae436aeb25adaae9ad7b7ae
Cesaly Garrett <cesalygarrett@gmail.com>
2020-06-27 16:02:30 -0500

    95% done, with help from Brandi Cotton.

---
+++ ghostposts/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from ghostposts import views
+
+urlpatterns = [
+    path('', views.index, name='home'),
+    path('post/', views.add_ghost_post),
+    path('boasts/', views.boasts),
+    path('roasts/', views.roasts),
+    path('like/<int:id>/', views.likes),
+    path('dislike/<int:id>/', views.dislikes),
+    path('most_liked/', views.most_liked),
+    path('least_liked/', views.least_liked),
+]
---
+++ ghostposts/views.py
@@ -0,0 +1,61 @@
+from django.shortcuts import render, reverse, HttpResponseRedirect
+
+from ghostposts.models import GhostPost
+from ghostposts.forms import GhostPostForm
+
+html = 'index.html'
+
+
+# Create your views here.
+def index(request):
+    data = GhostPost.objects.all().order_by('-submission_time')
+    return render(request, html, {'data': data})
+
+
+def add_ghost_post(request):
+    if request.method == 'POST':
+        form = GhostPostForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            GhostPost.objects.create(
+                boast_or_roast=data['boast_or_roast'],
+                post=data['post'],
+            )
+        return HttpResponseRedirect(reverse('home'))
+
+    form = GhostPostForm()
+    return render(request, 'post.html', {'form': form})
+
+
+def boasts(request):
+    data = GhostPost.objects.filter(boast_or_roast=True).order_by('-submission_time')
+    return render(request, html, {'data': data})
+
+
+def roasts(request):
+    data = GhostPost.objects.filter(boast_or_roast=False).order_by('-submission_time')
+    return render(request, html, {'data': data})
+
+
+def likes(request, id):
+    post = GhostPost.objects.get(id=id)
+    post.up_votes += 1
+    post.save()
+    return HttpResponseRedirect(reverse('home'))
+
+
+def dislikes(request, id):
+    post = GhostPost.objects.get(id=id)
+    post.down_votes += 1
+    post.save()
+    return HttpResponseRedirect(reverse('home'))
+
+
+def most_liked(request):
+    data = GhostPost.objects.order_by('-up_votes')
+    return render(request, html, {'data': data})
+
+
+def least_liked(request):
+    data = GhostPost.objects.order_by('up_votes')
+    return render(request, html, {'data': data})
